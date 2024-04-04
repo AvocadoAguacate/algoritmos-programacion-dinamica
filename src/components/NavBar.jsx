86b644439286ee01fcd704297a773113d95aefb8
AvocadoAguacate <estebanguzra@gmail.com>
2024-04-04 17:17:56 -0600

    ADD navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {React, useState} from 'react'
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import logo from '../assets/backpack.svg'
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div>NavBar</div>
-  )
+    <Navbar>
+      <NavbarBrand>
+        <img src='../assets/backpack.svg'/>
+        <p className="font-bold text-inherit">Algoritmos de programación dinámica</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem isActive>
+          <Link href="/" aria-current="page">
+            Algoritmos
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="/backpack" aria-current="page">
+            Contacto
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 }
 
 export default NavBar
