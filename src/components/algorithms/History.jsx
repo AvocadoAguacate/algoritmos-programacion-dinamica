a902011fe41affa9c4f0f6245fd149ffba8d229d
AvocadoAguacate <estebanguzra@gmail.com>
2024-04-15 20:57:13 -0600

    ADD flex to Floyd

--- src/components/algorithms/History.jsx
+++ src/components/algorithms/History.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
 
-export default function History({body, header}) {
+export default function History({body, header, classname}) {
   return (
-    <Card>
+    <Card className={classname}>
       <CardHeader>
         {header}
       </CardHeader>
--- src/pages/algorithms/Floyd.jsx
+++ src/pages/algorithms/Floyd.jsx
@@ -3,27 +3,36 @@ import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nex
 import History from '../../components/algorithms/History';
 
 function Floyd() {
+  // TODO: Grafo y Tabla con basis-full si el grafo es suficientemente grande
   return (
     <div>
-      <h1>Floyd</h1>
+      <div className='flex flex-wrap justify-start place-content-evenly'>
       <History 
       body='aquí la historia del algoritmo' 
-      header='Algoritmo Floyd ( Rutas óptimas )'/>
-      <Card>
+      header='Algoritmo Floyd ( Rutas óptimas )'
+      classname='basis-1/4 grow-0'
+      />
+      <Card className='basis-3/4 grow-0'>
         <CardHeader>
           Configuración
         </CardHeader>
       </Card>
-      <Card>
+      <Card className='basis-2/6'>
         <CardHeader>
           Grafo
         </CardHeader>
       </Card>
-      <Card>
+      <Card className='basis-4/6'>
         <CardHeader>
           Tabla
         </CardHeader>
       </Card>
+      <Card className='basis-full'>
+        <CardHeader>
+          Resultado
+        </CardHeader>
+      </Card>
+      </div>
     </div>
   )
 }
